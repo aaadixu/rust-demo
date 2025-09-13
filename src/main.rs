06cd4c9c1fb0a4d26b88dc05f76d123472b969af
zhixu <2533430882@qq.com>
2025-09-14 00:02:59 +0800

    [add] rust 闭包

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,26 @@ mod print_demo;
 mod basic_grammar;
 mod data_type;
 mod iterator;
+mod closure;
 
 use print_demo as printdemo;
 use data_type::data_type as datatype;
 use iterator::iterator_demo;
+use closure::closure_demo;
 
 fn main() {
     // rust 输出到命令行
     printdemo::print::print_demo();
-    
+
     // 基础语法
     basic_grammar::basic::basic_grammar();
-    
+
     // 数据类型
     datatype::data_type_demo();
-    
+
     // 迭代器
     iterator_demo::iterator_demo();
+    
+    // 闭包
+    closure_demo::closure_demo();
 }
